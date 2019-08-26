de34aa11e178ca521315cef95673f9f070e96fff
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-08-26 15:18:47 +0200

    Add csrf support
    Install csurf library

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../config/config');
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true })
 
-router.get('/', (req, res) => {
-  res.render('accounts/index', {metadata: config.METADATA, currentYear: (new Date).getFullYear()});
+router.get('/', csrfProtection, (req, res) => {
+  res.render('accounts/index', {metadata: config.METADATA, currentYear: (new Date).getFullYear(), csrf: req.csrfToken()});
 });
 
-router.get('/login', (req, res) => {
-  res.render('accounts/login', {metadata: config.METADATA, currentYear: (new Date).getFullYear()});
+router.get('/login', csrfProtection, (req, res) => {
+  res.render('accounts/login', {metadata: config.METADATA, currentYear: (new Date).getFullYear(), csrf: req.csrfToken()});
 });
 
-router.get('/register', (req, res) => {
-  res.render('accounts/register', {metadata: config.METADATA, currentYear: (new Date).getFullYear()});
+router.get('/register', csrfProtection, (req, res) => {
+  res.render('accounts/register', {metadata: config.METADATA, currentYear: (new Date).getFullYear(), csrf: req.csrfToken()});
 });
 
 module.exports = router;
