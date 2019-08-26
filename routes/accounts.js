cba0aa131d83fe54428f7d6369d3b09fe45d2b7e
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-08-26 14:03:35 +0200

    Add register and login pages

--- routes/accounts.js
+++ routes/accounts.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
   res.render('accounts/index', {metadata: config.METADATA, currentYear: (new Date).getFullYear()});
 });
 
+router.get('/login', (req, res) => {
+  res.render('accounts/login', {metadata: config.METADATA, currentYear: (new Date).getFullYear()});
+});
+
+router.get('/register', (req, res) => {
+  res.render('accounts/register', {metadata: config.METADATA, currentYear: (new Date).getFullYear()});
+});
+
 module.exports = router;
